dd2b6a811f6c60a325b4c8a3bb3242177b850015
Marcel Reboucas <marcel.reboucas@inlocomedia.com>
2017-09-26 17:54:42 -0300

    Removes papaparse

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ import React, {Component} from 'react';
 import {render} from 'react-dom';
 import MapGL from 'react-map-gl';
 import DeckGLOverlay from './deckgl-overlay.js';
-import Papa from 'papaparse';
 
-import { json as requestJson } from 'd3-request';
+import { json as requestJson, csv as requestCsv } from 'd3-request';
 
 // Set your mapbox token here
 const MAPBOX_TOKEN = 'pk.eyJ1IjoibWFyY2VscmVib3VjYXMiLCJhIjoiY2o3eGZkYm5rMTd2bDJ3bzJ4enN6czRkdSJ9.GtCF9r--onL2FkjK7ETj4A';
@@ -29,34 +28,30 @@ class Root extends Component {
       },
       data: null
     };
-  }
 
-  componentWillMount() {
-
-    // requestJson(DATA_URL, (error, response) => {
-    //   if (!error) {
-    //     this.setState({data: response});
-    //   }
-    // });
-
-    this.readFile(FILE_PATH, (error, response) => {
-
-      if (!error) {
-        var viewport = this.state.viewport;
-
-        if (response.length > 0) {
-          // Updates the viewport to be centered in the first point of the csv
-          viewport.latitude = response[0].lat;
-          viewport.longitude = response[0].lng;
-        }
-
-        this.setState({
-          data: this.filterResponse(response), 
-          viewport: viewport}
-        );
-      } else {
-        console.debug(error);
+    requestCsv(FILE_PATH, (d) => {
+      // for each row, convert numbers
+      return {
+        ...d,
+        timestamp: +d.timestamp,
+        lat: +d.lat,
+        lng: +d.lng,
+      };
+    }, (data) => {
+      console.log(data);
+      // for the full response
+      var viewport = this.state.viewport;
+      
+      if (data.length > 0) {
+        // Updates the viewport to be centered in the first point of the csv
+        viewport.latitude = data[0].lat;
+        viewport.longitude = data[0].lng;
       }
+
+      this.setState({
+        data: data, 
+        viewport: viewport}
+      );
     });
   }
 
@@ -68,29 +63,6 @@ class Root extends Component {
       )
     );
     return filtered;
-  }
-
-  readFile(file, onCompletion) {
-    // Reads the csv file as a string
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, true);
-    
-		rawFile.onreadystatechange = () => {
-      // Parses the csv string to a list of objects
-      Papa.parse(rawFile.responseText, {
-        header: true,
-        dynamicTyping: true,
-        complete: function(results) {
-          console.log(results);
-          onCompletion(null, results.data);
-        }
-      });
-    };
-
-    rawFile.onerror = (target, error) => {
-      onCompletion(error, null)
-    }
-    rawFile.send(null);
   }
 
   componentDidMount() {
